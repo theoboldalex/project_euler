af9c4293530bc2eb9a445e98c045291d6580321f
theoboldalex <theoboldalex@gmail.com>
2023-03-13 23:22:21 +0000

    01 - Multiples of 3 or 5

---
+++ multiples_of_3_or_5/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to multiples_of_3_or_5
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ multiples_of_3_or_5/src/multiples_of_3_or_5/core.clj
@@ -0,0 +1,18 @@
+(ns multiples-of-3-or-5.core
+  (:gen-class))
+
+;; If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+;; Find the sum of all the multiples of 3 or 5 below 1000.
+
+(defn -main
+  "Sum multiples of 3 or 5 below limit of 1000"
+  [& args]
+  (println 
+    (->> 
+      (range 1000)
+      (filter #(or (= 0 (mod % 5)) (= 0 (mod % 3))))
+      (reduce +)
+    )
+  )
+)
