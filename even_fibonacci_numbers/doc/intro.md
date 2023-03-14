50641ba55d64c90da608b8c2f0f16cc78295c210
theoboldalex <theoboldalex@gmail.com>
2023-03-14 00:12:48 +0000

    fibonacci evens sum

---
+++ even_fibonacci_numbers/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to even_fibonacci_numbers
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- multiples_of_3_or_5/src/multiples_of_3_or_5/core.clj
+++ multiples_of_3_or_5/src/multiples_of_3_or_5/core.clj
@@ -2,16 +2,15 @@
   (:gen-class))
 
 ;; If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-
 ;; Find the sum of all the multiples of 3 or 5 below 1000.
 
 (defn -main
-  "Sum multiples of 3 or 5 below limit of 1000"
+  "Sum all multiples of 3 or 5 below a limit of 1000"
   [& args]
   (println 
-    (->> 
+    (->>
       (range 1000)
-      (filter #(or (= 0 (mod % 5)) (= 0 (mod % 3))))
+      (filter #(or (= 0 (mod % 3)) (= 0 (mod % 5))))
       (reduce +)
     )
   )
